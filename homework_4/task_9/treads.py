42cefc436dc4eeb27c13c1327f9a7ba86ecb4f51
s.eremkin <ffr@list.ru>
2023-09-10 13:49:46 +0300

    homework_4

--- homework_4/task_9/treads.py
+++ homework_4/task_9/treads.py
@@ -1,18 +1,24 @@
+import requests
+import time
+import threading
+from homework_4.urls import urls
 
 
 def download(url):
-response = requests.get(url)
-filename = 'threading_' + url.replace('https://',
-'').replace('.', '_').replace('/', '') + '.html'
-with open(filename, "w", encoding='utf-8') as f:
-f.write(response.text)
-print(f"Downloaded {url} in {time.time()-start_time:.2f}
-seconds")
+    response = requests.get(url)
+    filename = url.replace('https://', '').split('/')[-1]
+    with open(f'images/{filename}', "wb") as f:
+        f.write(response.content)
+    print(f"Downloaded {url} in {time.time() - start_time:.2f}seconds")
+
+
 threads = []
 start_time = time.time()
-for url in urls:
-thread = threading.Thread(target=download, args=[url])
-threads.append(thread)
-thread.start()
-for thread in threads:
-thread.join()
+
+if __name__ == '__main__':
+    for url in urls:
+        thread = threading.Thread(target=download, args=[url])
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
